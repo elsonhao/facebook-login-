ba1123a584dcd57a73a8b4bd13f1b40fb17b3259
elson Huang <elson.huang@jamzoo.com.tw>
2017-07-15 17:42:39 +0800

    fb login button success

--- pixi pro/ViewController.swift
+++ pixi pro/ViewController.swift
@@ -7,17 +7,71 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,FBSDKLoginButtonDelegate {
+
+    @IBOutlet weak var facebookLogin: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        facebookLogin.readPermissions = ["public_profile", "email", "user_friends"]
+        facebookLogin.delegate = self
+        if (FBSDKAccessToken.current()) != nil{
+                        fetchProfile()
+        }
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("成功登入")
+        fetchProfile()
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    func fetchProfile(){
+        print("fetch profile")
+        
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start(completionHandler: {
+            connection, result, error -> Void in
+            
+            if error != nil {
+                print("longinerror =\(error)")
+            } else {
+                
+                if let resultNew = result as? [String:Any]{
+                    
+                    let email = resultNew["email"]  as! String
+                    print(email)
+                    
+                    let firstName = resultNew["first_name"] as! String
+                    print(firstName)
+                    
+                    let lastName = resultNew["last_name"] as! String
+                    print(lastName)
+                    
+                    if let picture = resultNew["picture"] as? NSDictionary,
+                        let data = picture["data"] as? NSDictionary,
+                        let url = data["url"] as? String {
+                        print(url) //臉書大頭貼的url, 再放入imageView內秀出來
+                    }
+                }
+            }
+        })
     }
 
 
